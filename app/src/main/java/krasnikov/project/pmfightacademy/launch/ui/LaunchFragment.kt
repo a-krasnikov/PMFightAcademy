48ac92c80920e192583c780eed759b674c4b6ba8
Dima Tkachenko <dimbook93@gmail.com>
2021-03-21 12:00:11 +0200

    added placeholder launch fragment

---
+++ app/src/main/java/krasnikov/project/pmfightacademy/launch/ui/LaunchFragment.kt
@@ -0,0 +1,7 @@
+package krasnikov.project.pmfightacademy.launch.ui
+
+import androidx.fragment.app.Fragment
+import krasnikov.project.pmfightacademy.R
+
+class LaunchFragment: Fragment(R.layout.fragment_launch) {
+}
