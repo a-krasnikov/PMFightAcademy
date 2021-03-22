a07cfe29fbca5ced1235e79ecb87c8d901bc84d6
a-krasnikov <andriikrasnikov@gmail.com>
2021-03-22 16:49:26 +0200

    Add base fragment and viewmodel

--- app/src/main/java/krasnikov/project/pmfightacademy/coaches/ui/CoachesFragment.kt
+++ app/src/main/java/krasnikov/project/pmfightacademy/coaches/ui/CoachesFragment.kt
@@ -1,35 +1,26 @@
 package krasnikov.project.pmfightacademy.coaches.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.onEach
+import krasnikov.project.pmfightacademy.app.ui.base.BaseFragment
 import krasnikov.project.pmfightacademy.databinding.FragmentCoachesBinding
 import krasnikov.project.pmfightacademy.utils.launchWhenStarted
 
 @AndroidEntryPoint
-class CoachesFragment : Fragment() {
+class CoachesFragment : BaseFragment<CoachesViewModel, FragmentCoachesBinding>() {
 
-    private lateinit var binding: FragmentCoachesBinding
-
-    private val viewModel by viewModels<CoachesViewModel>()
+    override val viewModel by viewModels<CoachesViewModel>()
 
     private val adapter = CoachesAdapter {
         viewModel.loadNextData()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentCoachesBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun createViewBinding() {
+        mBinding = FragmentCoachesBinding.inflate(layoutInflater)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/krasnikov/project/pmfightacademy/coaches/ui/CoachesViewModel.kt
+++ app/src/main/java/krasnikov/project/pmfightacademy/coaches/ui/CoachesViewModel.kt
@@ -1,6 +1,5 @@
 package krasnikov.project.pmfightacademy.coaches.ui
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,13 +8,15 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import krasnikov.project.pmfightacademy.app.pagination.PaginationData
 import krasnikov.project.pmfightacademy.app.pagination.PaginationState
+import krasnikov.project.pmfightacademy.app.ui.base.BaseViewModel
 import krasnikov.project.pmfightacademy.coaches.data.Coach
 import krasnikov.project.pmfightacademy.coaches.data.CoachRepository
+import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
 class CoachesViewModel @Inject constructor(private val coachRepository: CoachRepository) :
-    ViewModel() {
+    BaseViewModel() {
 
     private val _contentCoaches = MutableStateFlow<PaginationData<Coach>>(
         PaginationData(
@@ -26,7 +27,7 @@ class CoachesViewModel @Inject constructor(private val coachRepository: CoachRep
     val contentCoaches = _contentCoaches.asStateFlow()
 
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(exceptionHandler) {
             coachRepository.flowData.collect {
                 _contentCoaches.value = PaginationData(it, PaginationState.Complete)
             }
@@ -34,9 +35,13 @@ class CoachesViewModel @Inject constructor(private val coachRepository: CoachRep
         loadNextData()
     }
 
+    override fun handleError(throwable: Throwable) {
+        _contentCoaches.value.stateToError(throwable as Exception)
+    }
+
     fun loadNextData() {
         _contentCoaches.value = _contentCoaches.value.stateToLoading()
-        viewModelScope.launch {
+        viewModelScope.launch(exceptionHandler) {
             coachRepository.loadNextData()
         }
     }
