30862966e073c0814a09065bca483d4c98521856
a-krasnikov <andriikrasnikov@gmail.com>
2021-03-26 17:13:25 +0200

    Fix date

--- app/src/main/java/krasnikov/project/pmfightacademy/utils/Extensions.kt
+++ app/src/main/java/krasnikov/project/pmfightacademy/utils/Extensions.kt
@@ -5,5 +5,5 @@ import com.prolificinteractive.materialcalendarview.CalendarDay
 private const val FORMAT_DAY_AND_MONTH = "%02d"
 
 fun CalendarDay.getFormattedDate(): String {
-    return "${FORMAT_DAY_AND_MONTH.format(month)}/${FORMAT_DAY_AND_MONTH.format(day)}/$year"
+    return "${FORMAT_DAY_AND_MONTH.format(month)}.${FORMAT_DAY_AND_MONTH.format(day)}.$year"
 }
